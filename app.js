8904b0f7c1c2c8c57e41f93c882d6f3e3d7b5be1
Aleksey <1aleksey.mazur@gmail.com>
2022-07-13 15:09:55 +0300

    create and require date.js module; create header and footer template, include to list.ejs; small style improvements

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
 const port = 3000;
 
 const app = express();
 
-let items = ["Buy Food", "Cook Food", "Eat Food"];
+const items = ["Buy Food", "Cook Food", "Eat Food"];
+const workItems = [];
 
 app.set("view engine", "ejs");
 
@@ -12,25 +14,29 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  const today = new Date();
+  const day = date.getDate();
 
-  const options = {
-    weekday: "long",
-    day: "numeric",
-    month: "long",
-  };
-
-  const day = today.toLocaleDateString("en-UA", options);
-
-  res.render("list", { kindOfDay: day, newListItems: items });
+  res.render("list", { listTitle: day, newListItems: items });
 });
 
 app.post("/", (req, res) => {
   const item = req.body.newItem;
 
-  items.push(item);
+  if (req.body.list === "Work List") {
+    workItems.push(item);
+    res.redirect("/work");
+  } else {
+    items.push(item);
+    res.redirect("/");
+  }
+});
 
-  res.redirect("/");
+app.get("/work", (req, res) => {
+  res.render("list", { listTitle: "Work List", newListItems: workItems });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about");
 });
 
 app.listen(port, () => {
